2b201e2fb3cc4560687c68de19c709e11ddbcfe3
lillitaz <lilli.weitzer@gmail.com>
2023-04-29 11:30:52 +0200

    initial setup

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">       
+        <a
+          className="App-Link"
+          href=""
+        >
+          Lilli Weitzer
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
