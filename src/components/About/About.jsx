d293e79ab4eba397d9fa5255b4b7244a73ed5527
lillitaz <lilli.weitzer@gmail.com>
2023-04-29 22:59:16 +0200

    content update

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -33,7 +33,7 @@ const About = () => {
                     </div>
                     <p>
                         {" "}
-                        As a participant in a project-based training course in web development,
+                        As a participant in a project-based training course in full stack development,
                         I have already successfully completed several projects and now want to apply and expand my knowledge in practice.
                         I have very good knowledge of HTML, CSS, and JavaScript, as well as experience with the MERN stack. In my training,
                         I have intensively dealt with the development of web applications and the creation of performant and maintainable code.
